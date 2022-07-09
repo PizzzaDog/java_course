250150541a5cad1d09b3438dea785ee310f9fe66
PizzzaDog <7883494@gmail.com>
2022-07-09 13:48:59 +0300

    homework

--- src/main/java/org/axample/lesson1/homework/Task.java
+++ src/main/java/org/axample/lesson1/homework/Task.java
@@ -2,6 +2,9 @@ package org.axample.lesson1.homework;
 
 public class Task {
     /**
+     * 1. Сделать ArrayList
+     * 2. Написать тесты на методы своей реализации
+     * 3. Изучить коллекции
      *
      */
 }
