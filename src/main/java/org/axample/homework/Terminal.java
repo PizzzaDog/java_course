5bea14cb30f0c0a93f536ea1087f4194c12fedfb
PizzzaDog <7883494@gmail.com>
2022-07-23 13:35:07 +0300

    A little fixes

--- src/main/java/org/axample/homework/Terminal.java
+++ src/main/java/org/axample/homework/Terminal.java
@@ -1,9 +1,14 @@
 package org.axample.homework;
 
 public interface Terminal {
+
     void deposit(int bankNumber, double sum);
+
     void deposit(double sum);
+
     void topUpPhone(int phoneNumber, double sum);
+
     Double getBalance();
+
     void setBankNumber(int bankNumber);
 }
